36efd2e667954fdaf3a652315cb85c145eeaab66
Tim Julian Hubert Wiebe <tim.hubert@avantys.de>
2026-05-26 20:30:22 -0300

    Se ha añadido un módulo funcional.

--- modulo_funcional.py
+++ modulo_funcional.py
@@ -2,12 +2,30 @@ from tpi2_G17_fernandez_hubert_maldonado import *
 
 def titulos_leidos(biblioteca: Biblioteca) -> list:
     """Retorna una lista con los títulos de los libros leídos usando filter() y map()."""
-    pass
+
+    libros = biblioteca.libros #para el linter
+
+    leidos = filter(lambda libro: libro.leido, libros)
+    titulos = map(lambda libro: libros.titulo, leidos)
+    
+    return list(titulos)
+    
 
 def resumen_coleccion(biblioteca: Biblioteca) -> str:
     """Genera un string con el resumen de cada libro usando map() y una función lambda o auxiliar"""
-    pass
+
+    libros = biblioteca.libros
+
+    def formatear_libro(libro):
+        estado = '✔' if libro.leido else '✘'
+        return f'"{libro.titulo}" — {libro.autor} ({libro.anio}) [{libro.genero}] {estado}'
+    
+    resumenes = map(formatear_libro, libros)
+    return "\n".join(resumenes)
 
 def libros_por_anio(biblioteca: Biblioteca) -> list:
     """Retorna la lista de libros ordenada por año usando sorted() con key=lambda."""
-    pass
+
+    libros = biblioteca.libros
+
+    return sorted(libros, key=lambda libro: libro.anio)
